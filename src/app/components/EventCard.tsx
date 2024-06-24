3bfabe60a1e04c7defc7ad3cca363abf4670ce89
Laís Matie <lmatiehara@gmail.com>
2024-06-24 16:15:24 -0400

    [NEW] start list events and define components

---
+++ src/app/components/EventCard.tsx
@@ -0,0 +1,26 @@
+import { PropsWithChildren } from "react";
+import { EventModel } from "../model";
+
+export type EventCardProps = {
+  event: EventModel
+}
+
+export function EventCard(props: EventCardProps) {
+  return (
+    <div className="flex w-[277px] flex-col rounded-2xl bg-secondary">
+    <img src={props.event.image_url} alt={props.event.name} />
+    <div className="flex flex-col gap-y-2 px-4 py-6">
+      <p className="text-sm uppercase text-subtitle">
+        {new Date(props.event.date).toLocaleDateString("pt-BR", {
+          weekday: "long",
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+        })}
+      </p>
+      <p className="font-semibold">{props.event.name}</p>
+      <p className="text-sm font-normal">{props.event.location}</p>
+    </div>
+  </div>
+  )
+}
---
+++ src/app/components/Title.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from "react";
+
+export type TitleProps = {
+  className?: string
+}
+
+export function Title(props: PropsWithChildren<TitleProps>) {
+  return (
+    <h1 className={`text-left text-[24px] font-semibold ${props.className}`}>Página 1</h1>
+  )
+}
