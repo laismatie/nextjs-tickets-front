dd312291d5af526e911e1d94d46f906b4b71f087
Laís Matie <lmatiehara@gmail.com>
2024-06-24 17:55:21 -0400

    [NEW] mock api to list events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,17 @@
-import Image from "next/image";
 import { Title } from "./components/Title";
 import { EventCard } from "./components/EventCard";
 import { EventModel } from "./model";
 
-export default function HomePage() {
-  const events: EventModel[] = [
-    {
-      id: "1",
-      name: "Mulheres que Mudam o Mundo com a Tecnologia",
-      organization: "Developer Girls",
-      date: "2024-03-16T00:00:00.000Z",
-      location: "Campo Grande, MS",
-      price: 0,
-      rating: "",
-      image_url: "https://images.sympla.com.br/65c2f14eac7a9-lg.png"
-    },
-    {
-      id: "1",
-      name: "Mulheres que Mudam o Mundo com a Tecnologia",
-      organization: "Developer Girls",
-      date: "2024-03-16T00:00:00.000Z",
-      location: "Campo Grande, MS",
-      price: 0,
-      rating: "",
-      image_url: "https://images.sympla.com.br/65c2f14eac7a9-lg.png"
-    },
-    {
-      id: "1",
-      name: "Mulheres que Mudam o Mundo com a Tecnologia",
-      organization: "Developer Girls",
-      date: "2024-03-16T00:00:00.000Z",
-      location: "Campo Grande, MS",
-      price: 0,
-      rating: "",
-      image_url: "https://images.sympla.com.br/65c2f14eac7a9-lg.png"
-    },
-  ];
+export async function getEvents(): Promise<EventModel[]> {
+  const response = await fetch('http://localhost:8080/events',{
+    cache: "no-store",
+  });
+
+  return (await response.json()).events;
+}
+
+export default async function HomePage() {
+  const events = await getEvents();
   return (
     <main className="mt-10 flex flex-col">
     <Title>Eventos disponíveis</Title>
